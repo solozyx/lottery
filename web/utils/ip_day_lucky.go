13f2081073fcb8acbb19c75a434e67ebfb182b11
solozyx <zhaoyingxin@aliyun.com>
2019-08-05 19:51:01 +0800

    【重要】定时任务 每日 0 时清理 ips 分段数据

--- web/utils/ip_day_lucky.go
+++ web/utils/ip_day_lucky.go
@@ -15,6 +15,28 @@ import (
 
 const ipFrameSize = 2
 
+// init 程序启动时调用1次 清理ip分段数据
+func init() {
+	// 本地开发测试的时候，每次启动归零
+	resetGroupIpList()
+}
+
+// TODO : 计划任务,定点在每日 0 时执行
+//  重置单机IP今天次数
+func resetGroupIpList() {
+	log.Println("ip_day_lucky.resetGroupIpList start")
+	cacheObj := datasource.InstanceCache()
+	// 对2个段的数据 循环清理
+	for i := 0; i < ipFrameSize; i++ {
+		key := fmt.Sprintf("day_ips_%d", i)
+		cacheObj.Do("DEL", key)
+	}
+	log.Println("ip_day_lucky.resetGroupIpList stop")
+
+	// IP当天的统计数,次日零点清空,设置定时器
+	duration := comm.NextDayDuration()
+	time.AfterFunc(duration, resetGroupIpList)
+}
 
 // 今天的IP抽奖次数递增，返回递增后的数值
 func IncrIpLuckyNum(strIp string) int64 {
